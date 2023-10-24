9ebd6213864588b6e0488659df8f9080b3696d11
Cock-Old <106105317+Cock-Old@users.noreply.github.com>
2023-10-24 10:58:32 +0300

    VVK laba 3 added

---
+++ VVK/VVK laba 3/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double l0 = 0.1;
+        List<double> lambda = new List<double> { 0.37037, 0.085185, 0.1, 0.085185 };
+        List<double> v = new List<double> { 0.43, 0.2, 0.67, 0.55 };
+        List<double> a = lambda.ConvertAll(l => Math.Round(l / l0, 5));
+        List<int> k = new List<int> { 2, 1, 2, 2 };
+        double ou = 3.74631;
+
+        for (int i = 0; i < lambda.Count; i++)
+        {
+            Console.WriteLine("l(" + i + ") = " + lambda[i]);
+        }
+
+        double sum = 0;
+        for (int i = 0; i < a.Count; i++)
+        {
+            Console.WriteLine("a(" + i + ") = " + a[i]);
+        }
+        Console.WriteLine("a = " + (a[0] + lambda[1]));
+
+        for (int i = 0; i < lambda.Count; i++)
+        {
+            Console.WriteLine("stacionar(" + i + ") = " + k[i] / (a[i] * v[i]));
+        }
+
+        List<double> b = new List<double>();
+        for (int i = 0; i < lambda.Count; i++)
+        {
+            double bi = lambda[i] * v[i];
+            Console.WriteLine("b(" + i + ") = " + bi);
+            b.Add(bi);
+        }
+
+        List<double> pi = new List<double>();
+        double PI = 1;
+        for (int i = 0; i < k.Count; i++)
+        {
+            double p;
+            if (k[i] == 1)
+            {
+                p = 1 - b[i];
+                Console.WriteLine("P(" + i + ") = " + p);
+            }
+            else
+            {
+                p = 1 / (Math.Pow(b[i], 2) / (2 * (1 - b[i] / 2)) + 1 + b[i]);
+                Console.WriteLine("P(" + i + ") = " + p);
+            }
+            pi.Add(Math.Round(p, 5));
+            PI *= p;
+        }
+
+        Console.WriteLine("PI = " + PI);
+
+        List<double> l = new List<double>();
+        for (int i = 0; i < b.Count; i++)
+        {
+            double li = (Math.Pow(b[i], k[i] + 1) / (k[i] * k[i] * Math.Pow((1 - b[i] / k[i]), 2))) * pi[i];
+            Console.WriteLine("l(" + i + ") = " + li);
+            l.Add(Math.Round(li, 5));
+        }
+        sum = 0;
+        foreach(double tmp in l)
+        {
+            sum = sum + tmp;
+        }
+        Console.WriteLine("L = " + sum);
+
+        List<double> ro = new List<double>();
+        for (int i = 0; i < b.Count; i++)
+        {
+            double roi = b[i] / k[i];
+            ro.Add(roi);
+        }
+
+        List<double> m = new List<double>();
+        for (int i = 0; i < ro.Count; i++)
+        {
+            double mi = l[i] + b[i];
+            Console.WriteLine("m(" + i + ") = " + mi);
+            m.Add(Math.Round(mi, 5));
+        }
+        sum = 0;
+        foreach (double tmp in m)
+        {
+            sum = sum + tmp;
+        }
+        Console.WriteLine("M = " + sum);
+
+        List<double> w = new List<double>();
+        for (int i = 0; i < l.Count; i++)
+        {
+            double wi = l[i] / lambda[i];
+            Console.WriteLine("w(" + i + ") = " + wi);
+            w.Add(wi);
+        }
+        Console.WriteLine("W = " + a.Zip(w, (ai, wi) => ai * wi).Sum());
+
+        List<double> u = new List<double>();
+        for (int i = 0; i < m.Count; i++)
+        {
+            double ui = m[i] / lambda[i];
+            Console.WriteLine("u(" + i + ") = " + ui);
+            u.Add(ui);
+        }
+        double totalU = a.Zip(u, (ai, ui) => ai * ui).Sum();
+        Console.WriteLine("U = " + totalU);
+
+        double du = 100 - ((totalU * 100) / ou);
+        Console.WriteLine("du = " + du);
+    }
+}
