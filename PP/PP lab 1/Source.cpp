9e8b24ce5018af2b14133d1eeb345ccd725fefa8
Cock-Old <106105317+Cock-Old@users.noreply.github.com>
2024-07-08 15:06:31 +0300

    PP lab 1 modified

--- PP/PP lab 1/Source.cpp
+++ PP/PP lab 1/Source.cpp
@@ -38,23 +38,23 @@ size_t colorize(const graph_t& graph) {
     size_t orders_count = 500;
     vector<vector<size_t>> orders(orders_count);
 
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < size; ++i) {                //O(size)
         order[i] = i;
     }
 
-    for (auto& ord : orders) {
+    for (auto& ord : orders) {                         //O(orders_count)
         random_shuffle(order.begin(), order.end());
         ord = order;
     }
 
     size_t min = graph.size();
-    for (const auto& ord : orders) {
-        vector<size_t> colored(size, 0);
-        vector<size_t> colors(size, 0);
-        vector<size_t> used_colors(size, 0);
+    for (const auto& ord : orders) {                   //O(orders_count)
+        vector<size_t> colored(size, 0);     //закрашеные вершины
+        vector<size_t> colors(size, 0);      //цвета вершин
+        vector<size_t> used_colors(size, 0); //использованые цвета
 
-        for (size_t v : ord) {
-            for (auto to : graph[v]) {
+        for (size_t v : ord) {                         //O(size)
+            for (auto to : graph[v]) {                 //O(size)
                 if (colored[to]) {
                     used_colors[colors[to]] = 1; //этот цвет занят
                 }
@@ -68,7 +68,59 @@ size_t colorize(const graph_t& graph) {
         min = std::min(min, colors_count);
     }
 
-    return min;
+    return min; //O(size + orders_count + orders_count * (size * size)) = O(orders_count * size^2)
+}
+
+size_t colorize2(const graph_t& graph) {
+    size_t size = graph.size();
+    vector<size_t> order(size);
+    size_t orders_count = 500;
+    vector<vector<size_t>> orders(orders_count);
+
+    for (size_t i = 0; i < size; ++i) {                                                                                     //O(size)
+        order[i] = i;
+    }
+
+    for (auto& ord : orders) {                                                                                              //O(orders_count)
+        random_shuffle(order.begin(), order.end());
+        ord = order;
+    }
+
+    size_t min = graph.size();
+    for (const auto& ord : orders) {                                                                                        //O(orders_count)
+        vector<int> result(size, -1);         // закрашеные вершины
+        vector<bool> available(size, false);  // доступные дли исп цвета
+
+        result[0] = 0;                        // Assign the first color to the first vertex
+
+        for (size_t u : ord) {                // Assign colors to the remaining vertices                                    //O(size)
+
+            for (int adj : graph[u]) {        // Process all adjacent vertices and mark their colors as unavailable         //O(size)
+                if (result[adj] != -1) {
+                    available[result[adj]] = true;
+                }
+            }
+
+            int color;
+            for (color = 0; color < size; ++color) {  // Find the first available color                                     //O(size)
+                if (!available[color]) {
+                    break;
+                }
+            }
+
+            result[u] = color;                        // Assign the found color to the current vertex
+
+            for (int adj : graph[u]) {                // Reset the values back to false for the next iteration              //O(size)
+                if (result[adj] != -1) {
+                    available[result[adj]] = false;
+                }
+            }
+        }
+        size_t colors_count = 1 + *max_element(result.begin(), result.end());
+        min = std::min(min, colors_count);
+    }
+
+    return min; //O(size + orders_count + orders_count * (size * (size + size + size))) = O(orders_count * 3size^2)
 }
 
 int main() {
@@ -84,6 +136,22 @@ int main() {
         auto start = chrono::system_clock::now();
 
         auto res = colorize(graph);
+
+        auto stop = chrono::system_clock::now();
+
+        auto time = duration_cast<milliseconds>(stop - start).count();
+
+        output << res << endl;
+        cout << time << endl;
+    }
+
+    input.seekg(0, ios::beg);
+    input >> cnt;
+    for (size_t i = 0; i < cnt; ++i) {
+        input >> graph;
+        auto start = chrono::system_clock::now();
+
+        auto res = colorize2(graph);
 
         auto stop = chrono::system_clock::now();
 
