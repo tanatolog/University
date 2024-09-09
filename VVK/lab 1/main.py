275568a2fd9b0905bbb6b010ff87d1635b80bbfc
Tanatolog <106105317+tanatolog@users.noreply.github.com>
2024-09-10 01:23:24 +0300

    VVK lab 1 added

---
+++ VVK/lab 1/main.py
@@ -0,0 +1,43 @@
+from math import factorial as fact
+
+p_proc = 0.965
+p_mem = 0.96
+p_io = 0.88
+
+n_cpu = 1   #number cpus in one line
+n_mem = 8   #number mems in one line
+n_io = 4    #number ios in one line
+
+t4_reserve_mem = 1
+t4_reserve_io = 1
+
+t5_reserve_mem = 2
+t5_reserve_io = 2
+
+def f(p, n, q):
+    return 1 - (1  - p ** n) ** q
+
+def C(m, n):
+    return fact(n) / (fact(m) * fact(n - m))
+
+def P(m, n, p):
+    return C(m, n) * (p ** m) * ((1 - p) ** (n - m))
+
+def R(m, n, p):
+    return 1 - sum(P(i, n, p) for i in range(0, m))
+
+task0 = f(0.9, 1, 2)
+
+task1 = p_proc ** n_cpu * p_mem ** n_mem * p_io ** n_io
+
+task2 = f(p_proc, n_cpu, 2) * f(p_mem, n_mem, 2) * f(p_io, n_io, 2)
+
+task3 = f(p_proc, n_cpu, 3) * f(p_mem, n_mem, 3) * f(p_io, n_io, 3)
+
+task4 = p_proc ** 2 * R(n_mem, n_mem + t4_reserve_mem, p_mem) * R(n_io, n_io + t4_reserve_io, p_io)
+
+task5 = p_proc ** 2 * R(n_mem, n_mem + t5_reserve_mem, p_mem) * R(n_io, n_io + t5_reserve_io, p_io)
+
+task6 = p_proc ** 2 * R(n_mem, n_mem + n_mem, p_mem) * R(n_io, n_io + n_io, p_io)
+
+print(round(task6, 6))
