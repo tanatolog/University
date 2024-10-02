fec822a08e78797dca964ee6843ad80ca4b44a88
Tanatolog <106105317+tanatolog@users.noreply.github.com>
2024-10-02 03:45:51 +0300

    TPR lab 2 added

---
+++ TPR/лаб 2/t_7.m
@@ -0,0 +1,19 @@
+function main_script()
+    % Основной код
+    [X, Y] = meshgrid(-1:1, 1:3);
+    Z = log(1 + X.^2 + Y.^2).^2 + (X - Y - 1).^2;
+    plot3(X, Y, Z)
+
+    % Использование fminsearch
+    [xmin, minf] = fminsearch(@Fxy, [2; 2]);
+
+    % Вывод результатов
+    xmin
+    minf
+
+    % Вложенная функция
+    function f = Fxy(x)
+        x = x(:);  % Преобразование в столбец
+        f = log(1 + x(1)^2 + x(2)^2)^2 + (x(1) - x(2) - 1)^2;
+    end
+end
