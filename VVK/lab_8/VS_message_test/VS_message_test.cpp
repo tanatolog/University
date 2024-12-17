81a6424169f5740f65c3241a0384d3b6f06bb03a
Tanatolog <106105317+tanatolog@users.noreply.github.com>
2024-12-17 06:15:46 +0300

    VVK lab 8 modified

--- VVK/lab_8/VS_message_test/VS_message_test.cpp
+++ VVK/lab_8/VS_message_test/VS_message_test.cpp
@@ -1,22 +1,44 @@
 ﻿#include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
     int rank, size;
+    int send_data = 123, recv_data;
 
-    MPI_Init(&argc, &argv); // Инициализация MPI
+    // Инициализация MPI
+    MPI_Init(&argc, &argv);
 
-    // Получение ранга процесса и общего числа процессов
+    // Получение ранга и размера коммуникатора
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    // Проверяем, что запущено хотя бы два процесса
+    if (size < 2) {
+        printf("Error: need more than 2 processes\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processes\n", processor_name, rank, size);
+    MPI_Bcast(&send_data, 1, MPI_INT, 0, MPI_COMM_WORLD);  // Рассылка данных всем от процесса 0
 
-    MPI_Finalize(); // Finalize the MPI environment.
+    // Процесс с рангом 0 отправляет данные процессу с рангом 1
+    if (rank == 0) {
+        printf("Proc %d: send (%d) to proc 1\n", rank, send_data);
+        MPI_Send(&send_data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);  // Отправка данных
+    }
+    else if (rank == 1) {
+        MPI_Recv(&recv_data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);  // Получение данных
+        printf("Proc %d: recv (%d) to proc 0\n", rank, recv_data);
+    }
+
+    // Барьер: синхронизация процессов
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Все процессы выполнят этот вывод, когда дойдут до барьера
+    printf("Proc %d: barrier passed\n", rank);
+
+    // Завершаем работу с MPI
+    MPI_Finalize();
+
+    return 0;
 }
